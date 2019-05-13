b7412a4954d4da74625571f4cd11b9885103d664
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2019-05-13 03:14:36 +0200

    First commit

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import './App.css';
+
+function Header() {
+  return (
+      <header className="header">
+        <h1 className="title">
+          <span>Emmanuelle</span> 
+          <span>Jaldorau</span>
+          <p className="legend">web developer</p>
+        </h1>        
+        
+        <FontAwesomeIcon className="arrow" icon="chevron-down"/>
+      </header>
+  );
+}
+
+export default Header;
