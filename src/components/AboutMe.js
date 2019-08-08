4af81200a08491cf973083f94978ec6ee593ddaa
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2019-08-08 04:10:12 +0200

    style(aboutme): finalized mobile display

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,13 +6,13 @@ import profile from '../images/profilepic.jpeg';
 function AboutMe() {
   return (
       <section className="presentation">
-        <h2 className="title-presentation">ABOUT ME</h2>
-        <div className="content-presentation">
-          <div className="text-presentation">
+          <img src={profile} alt="profile" class="img-presentation"/> 
+          <div className="title-presentation">
+            <h2>ABOUT ME</h2>
+          </div>
+          <div class="text-presentation">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
           </div>
-          <img src={profile} alt="profile" />
-        </div>
       </section>
   );
 }
