9962e0cf8d16cdd9eb8fd3bcec6eaeda606f4d1f
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2019-08-10 17:55:14 +0200

    feat(skills): created a new section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <main>
      <Header />
      <AboutMe />
+     <Skills />
     </main>
   );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../App.css';
+
+
+function Skills() {
+  return (
+      <section className="skills">
+          <h2>WHAT I WORK WITH</h2>
+          <div class="skills-tiles-list">
+            <div class="skills-tile">
+              <div>Icon</div>
+              <div>Title</div>
+            </div>
+          </div>
+      </section>
+  );
+}
+
+export default Skills;
