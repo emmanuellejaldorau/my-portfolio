281933c00c8b2e50c654837663eb03f87d2946cd
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2019-08-29 05:25:59 +0200

    feat(skills): implemented bar dispaly with useState

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import SkillsTile from './SkillsTile';
 import SkillsBanner from './SkillsBanner';
 
 
 function Skills() {
+  const [bannerDisplayed, displayBanner ] = useState(false);
+  const handleClick = () => displayBanner(bannerDisplayed ? false : true);
   return (
       <section className="skills">
           <h2>WHAT I WORK WITH</h2>
           <div className="skills-tiles-list">
-            <SkillsTile icon="code" name="FRONT-END" />
-            <SkillsBanner />
-            <SkillsTile icon="cogs" name="BACK-END" />
-            <SkillsTile icon="tools" name="CMS" />
-            <SkillsTile icon="clipboard-list" name="COLLABORATION" />
+            <div className="skills-tile" onClick={handleClick}>
+              <SkillsTile icon="code" name="FRONT-END" />
+            </div>
+            {bannerDisplayed && <SkillsBanner />}
+            <div className="skills-tile" onClick={handleClick}>
+              <SkillsTile icon="cogs" name="BACK-END" />
+            </div>
+            <div className="skills-tile" onClick={handleClick}>
+              <SkillsTile icon="tools" name="CMS" />
+            </div>
+            <div className="skills-tile" onClick={handleClick}>
+              <SkillsTile icon="clipboard-list" name="COLLABORATION" />
+            </div>
           </div>
       </section>
   );
--- src/components/SkillsTile.js
+++ src/components/SkillsTile.js
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function SkillsTile(props) {
   return (    
-    <div className="skills-tile">
+    <>
       <div className="skills-main-icon">
         <FontAwesomeIcon icon={props.icon}/>
       </div>
       <p>{props.name}</p>
-    </div>
+    </>
   );
 }
 
