2d1b597707bc08b24208074a7568e51ca6f00df4
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2019-08-31 22:39:55 +0200

    feat(skills-banner) : created banner for every category

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,25 +5,53 @@ import SkillsBanner from './SkillsBanner';
 
 
 function Skills() {
-  const [bannerDisplayed, displayBanner ] = useState(false);
-  const handleClick = () => displayBanner(bannerDisplayed ? false : true);
+  const [frontBannerDisplayed, displayFrontBanner ] = useState(false);
+  const frontBanner = [
+    {icon:"angular" , name: "Angular" },
+    {icon:"bootstrap", name:"Bootstrap"},
+    {icon:"html5", name: "HTML 5"},
+    {icon:"css3-alt", name:"CSS 3"},
+    {icon:"js", name:"Javascript"},
+  ]
+  const [backBannerDisplayed, displayBackBanner ] = useState(false);
+  const backBanner = [
+    {icon:"node" , name: "Node.js" },
+    {icon:"java", name:"Java"}
+  ]
+
+  const [cmsBannerDisplayed, displayCmsBanner ] = useState(false);
+  const cmsBanner = [
+    {icon: "wordpress" , name: "Wordpress"},
+    {icon:"salesforce" , name: "Salesforce"},
+  ]
+
+  const [projectBannerDisplayed, displayProjectBanner ] = useState(false);
+  const projectBanner = [
+    {icon: "git" , name: "Git"},
+    {icon:"github" , name: "Github"},
+    {icon:"jira" , name: "Jira"},
+  ]
+
   return (
       <section className="skills">
           <h2>WHAT I WORK WITH</h2>
           <div className="skills-tiles-list">
-            <div className="skills-tile" onClick={handleClick}>
+            <div className="skills-tile" onClick={() => displayFrontBanner(frontBannerDisplayed ? false : true)}>
               <SkillsTile icon="code" name="FRONT-END" />
             </div>
-            {bannerDisplayed && <SkillsBanner />}
-            <div className="skills-tile" onClick={handleClick}>
+            {frontBannerDisplayed && <SkillsBanner iconsBanner= {frontBanner} />}
+            <div className="skills-tile" onClick={() => displayBackBanner(backBannerDisplayed ? false : true)}>
               <SkillsTile icon="cogs" name="BACK-END" />
             </div>
-            <div className="skills-tile" onClick={handleClick}>
+            {backBannerDisplayed && <SkillsBanner iconsBanner= {backBanner} />}
+            <div className="skills-tile" onClick={() => displayCmsBanner(cmsBannerDisplayed ? false : true)}>
               <SkillsTile icon="tools" name="CMS" />
             </div>
-            <div className="skills-tile" onClick={handleClick}>
+            {cmsBannerDisplayed && <SkillsBanner iconsBanner= {cmsBanner} />}
+            <div className="skills-tile" onClick={() => displayProjectBanner(projectBannerDisplayed ? false : true)}>
               <SkillsTile icon="clipboard-list" name="COLLABORATION" />
             </div>
+            {projectBannerDisplayed && <SkillsBanner iconsBanner= {projectBanner} />}
           </div>
       </section>
   );
--- src/components/SkillsBanner.js
+++ src/components/SkillsBanner.js
@@ -10,30 +10,14 @@ function SkillsBanner(props) {
         <FontAwesomeIcon className="decoration" icon="caret-up" />
       </div>    
       <div className="skills-banner">
-        <div className="skills-group">
-          <FontAwesomeIcon icon={['fab', 'react']} />
-          <p>React</p>
-        </div>
-        <div className="skills-group">
-          <FontAwesomeIcon icon={['fab', 'angular']} />
-          <p>Angular</p>
-        </div>
-        <div className="skills-group">
-          <FontAwesomeIcon icon={['fab', 'bootstrap']} />
-          <p>Bootstrap</p>
-        </div>
-        <div className="skills-group">
-          <FontAwesomeIcon icon={['fab', 'html5']} />
-          <p>HTML 5</p>
-        </div>
-        <div className="skills-group">
-          <FontAwesomeIcon icon={['fab', 'css3-alt']} />
-          <p>CSS 3</p>
-        </div>
-        <div className="skills-group">
-          <FontAwesomeIcon icon={['fab', 'js']} />
-          <p>JS</p>
-        </div>
+
+      { props.iconsBanner.map( (e , i) => {
+        return <div key={i} className="skills-group">
+          <FontAwesomeIcon icon={['fab', e.icon]} />
+          <p>{e.name}</p>
+        </div>})
+      }
+
 
       </div>
       
