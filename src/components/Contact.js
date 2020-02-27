ace9552b2c6d16e2ad2448c9f55e8344b4735f75
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-02-28 00:43:02 +0100

    feat(contact): adjusted section for desktop

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,15 +10,15 @@ function Contact() {
       <section className="contact">
         <h2 className="contact-title">GET IN TOUCH</h2>
         <div>
-          <div>
+          <div className="contact-item">
             <FontAwesomeIcon className="contact-icon" icon="phone-square-alt" />
             +1 (702) 613-9391
           </div>
-          <div>
+          <div className="contact-item">
             <FontAwesomeIcon className="contact-icon" icon="envelope-square" />
             e.jaldorau@gmail.com
           </div>
-          <div>
+          <div className="contact-item">
             <FontAwesomeIcon className="contact-icon" icon={['fab', 'linkedin']} />
             www.linkedin.com/in/ejaldorau
           </div>
