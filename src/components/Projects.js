1e0b1010f684eb0c93e56979ebcaed7fad3ea2e4
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-02-28 20:43:56 +0100

    feat(projects): updated section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,10 +8,31 @@ function Projects() {
     <>
       <h2 className="projects-title">LAST PROJECTS</h2>
       <section className="projects">
-          <div className="star-project"></div> 
+          
+          <div className="star-project">
+            <a href="https://customreactapp.herokuapp.com/" target="_blank">
+              <div className="project-overlay">
+                <h2>PRISM</h2>
+              </div> 
+              </a>
+          </div>
+          
+          
           <div className="other-project-group">
-            <div className="other-project"></div>
-            <div className="other-project"></div>
+            <div className="other-project iron-sharer">
+              <a href="https://ironhack-sharer.herokuapp.com/" target="_blank">
+                <div className="project-overlay">
+                  <h2>IRON SHARER</h2>
+                </div> 
+              </a>
+            </div>
+            <div className="other-project back-to-school">
+              <a href="https://emmanuellejaldorau.github.io/game-project/" target="_blank">
+                <div className="project-overlay">
+                  <h2>BACK TO SCHOOL</h2>
+                </div> 
+              </a>
+            </div>
           </div>
       </section>
     </>
