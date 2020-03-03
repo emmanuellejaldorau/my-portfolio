6be95f837a99dec08dc227e4b83d58448fba8847
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-03-03 06:02:45 +0100

    Updates

---
+++ precache-manifest.dca8a456a0649e49683144d3f4211314.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94b859364f93b7ac2070e15319840b5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2a4670ec82824360c1cf",
+    "url": "/my-portfolio/static/css/main.7af67238.chunk.css"
+  },
+  {
+    "revision": "e3d79eaea32c475fa4ec",
+    "url": "/my-portfolio/static/js/2.76c29774.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/my-portfolio/static/js/2.76c29774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4670ec82824360c1cf",
+    "url": "/my-portfolio/static/js/main.2aa274bc.chunk.js"
+  },
+  {
+    "revision": "327500901c72eab58f18",
+    "url": "/my-portfolio/static/js/runtime-main.d4d0ac1e.js"
+  },
+  {
+    "revision": "64929ea27c886b4fbf425a8542181cc9",
+    "url": "/my-portfolio/static/media/Playlist-Script.ttf.64929ea2.svg"
+  },
+  {
+    "revision": "77387fded0acce2803ab74f3cd14f475",
+    "url": "/my-portfolio/static/media/Playlist-Script.ttf.77387fde.woff"
+  },
+  {
+    "revision": "c750577fcdf5ccd925e7d2abec316825",
+    "url": "/my-portfolio/static/media/Playlist-Script.ttf.c750577f.eot"
+  },
+  {
+    "revision": "5a02b67981c6c02c05ac8f5860b5e9e2",
+    "url": "/my-portfolio/static/media/back-to-school.5a02b679.jpg"
+  },
+  {
+    "revision": "c287d565d445cf6067576e38be5d399f",
+    "url": "/my-portfolio/static/media/ironshaker.c287d565.jpg"
+  },
+  {
+    "revision": "0d2a7e478ddb03c5423f4b4b8185968d",
+    "url": "/my-portfolio/static/media/main-image.0d2a7e47.jpg"
+  },
+  {
+    "revision": "9601ee7a8e3f733fadffe65d447ee732",
+    "url": "/my-portfolio/static/media/prism.9601ee7a.jpg"
+  }
+]);
