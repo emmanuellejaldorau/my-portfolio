36b66d020983eaf48425be7ab39c8df3e6af9bc8
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-03-06 05:27:08 +0100

    feat(about-me):added cv link

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,6 +11,7 @@ function AboutMe() {
           </div>
           <div className="text-presentation">
             <p>I am a Full-stack developer with a background in digital project management and CMS contribution. Detail oriented and fast-learner, I am highly-motivated to reinvest the skills I acquired and expand my knowledge working with an outstanding team. I am looking for a position in NYC starting July 2020.</p>
+            <a href="/images/resume.pdf" class="cv-button" download>Download CV</a>
           </div>
       </section>
   );
