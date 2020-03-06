e55157293f003f4596bf51b3b601ed28c69afc87
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-03-06 05:38:57 +0100

    fix(general): corrected syntax

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,13 +16,13 @@ function Contact() {
           </div>
           <div className="contact-item">
             <FontAwesomeIcon className="contact-icon" icon="envelope-square" />
-            <a href="mailto:e.jaldorau@gmail.com" class="link">
+            <a href="mailto:e.jaldorau@gmail.com" className="link">
               e.jaldorau@gmail.com
             </a>
           </div>
           <div className="contact-item">
             <FontAwesomeIcon className="contact-icon" icon={['fab', 'linkedin']} />
-            <a href="https://www.linkedin.com/in/ejaldorau/?locale=en_US" target="_blank" rel="noopener noreferrer" class="link">
+            <a href="https://www.linkedin.com/in/ejaldorau/?locale=en_US" target="_blank" rel="noopener noreferrer" className="link">
               linkedin.com/in/ejaldorau
             </a>
           </div>
